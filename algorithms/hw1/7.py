cf94625ee6fc247de8808fe732c88befc9f0350a
vagrant <vagrant@vagrant.vm>
2021-07-28 11:13:22 +0000

    ...

--- algorithms/hw1/7.py
+++
@@ -1,8 +0,0 @@
-a = float(input('input first side: '))
-b = float(input('input second side: '))
-c = float(input('input third side: '))
-
-if 0 < a < b + c and 0 < b < a + c and 0 < c < a + b:
-    print('triangle is possible')
-else:
-    print('triangle is not possible')
--- algorithms/hw1/8.py
+++
@@ -1,6 +0,0 @@
-y = int(input('input the year: '))
-
-if (y % 100 != 0 and y % 4 == 0) or (y % 100 == 0 and y % 400 == 0):
-    print('leap year')
-else:
-    print('common year')
